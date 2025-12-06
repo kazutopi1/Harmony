2fb00195128486368c79b9f9aed617d3448d9fe0
Kazu Topi <kazutopi1@gmail.com>
2025-12-06 22:08:30 +0800

    Update Retaliate.cs

--- Retaliate.cs
+++ Retaliate.cs
@@ -1,18 +1,21 @@
 using StardewValley;
-using StardewValley.Buffs;
+using StardewValley.Tools;
 using StardewModdingAPI;
-using StardewModdingAPI.Events;
-using MonoGame.Framework;
 using HarmonyLib;
+using StardewValley.Buffs;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Atttttackk
+namespace Reta
 {
-    public class ThisWillWorkIPromise : Mod
+    public class Liate : Mod
     {
-        private const int Threshhold = 50; // takeDamage() is called multiple times in a single hit thats why its set to 50
+        private const double playerIFrames = 1200.0;
 
-        private static int hitCount = 0;
+        private static double lastDamaged = -1.0;
+
+        private static int damageTaken = 0;
+
+        private const int Threshold = 7;
 
         public override void Entry(IModHelper helper)
         {
@@ -20,35 +23,50 @@ namespace Atttttackk
 
             harmony.Patch(
                 original: AccessTools.Method(typeof(Farmer), nameof(Farmer.takeDamage)),
-                postfix: new HarmonyMethod(typeof(ThisWillWorkIPromise), nameof(ThisWillWorkIPromise.Postfix))
+                postfix: new HarmonyMethod(typeof(Liate), nameof(Liate.Postfix))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(MeleeWeapon), nameof(MeleeWeapon.doSwipe)),
+                postfix: new HarmonyMethod(typeof(Liate), nameof(Liate.Postfix2))
             );
         }
-        private static Buff BuffSetup()
+        private static Buff Retaliate()
         {
             Buff Retaliate = new Buff(
                 id: "df.retaliate",
                 displayName: "Retaliate",
                 iconTexture: Game1.content.Load<Texture2D>("TileSheets/BuffsIcons"),
                 iconSheetIndex: 11,
-                duration: 1_000,
+                duration: Buff.ENDLESS,
                 effects: new BuffEffects()
                 {
-                    CriticalPowerMultiplier = { 2 },
-                    CriticalChanceMultiplier = { 1000 },
-                    WeaponSpeedMultiplier = { 10 },
+                    Attack = { 50 },
+                    WeaponSpeedMultiplier = { 10 }
                 }
             );
             return Retaliate;
         }
-        private static void Postfix(Farmer __instance, int damage)
+        private static void Postfix()
         {
-            hitCount += 1;
+            double timeNow = Game1.currentGameTime.TotalGameTime.TotalMilliseconds;
 
-            if (hitCount >= Threshhold)
+            if (timeNow - lastDamaged >= playerIFrames)
             {
-                __instance.applyBuff(BuffSetup());
-                hitCount = 0;
+                lastDamaged = timeNow;
+
+                damageTaken += 1;
+
+                if (damageTaken >= Threshold
+                    && !Game1.player.hasBuff("df.retaliate"))
+                {
+                    Game1.player.applyBuff(Retaliate());
+                    damageTaken = 0;
+                }
             }
+        }
+        private static void Postfix2()
+        {
+            Game1.player.buffs.Remove("df.retaliate");
         }
     }
 }
